92ab3250a8f8f72fcbe29e0c9e4702fb0cab6832
wilhelmpaulm <wilhelmpaulm@gmail.com>
2016-02-10 15:16:03 +0800

    ADD: new stuff for py

---
+++ py/01.py
@@ -0,0 +1,13 @@
+# def sample():
+#    number  = input("please enter a number: ")
+#    return number
+# def sum(a, b):
+#    return a + b
+# @TODO: this is a sample todo
+# a = int(sample())
+# b = int(sample())
+# c = sum(a, b)
+# print("the sum is {0} bang bang".format(c))
+samples = ["this", "is", "an", "awesome", "list"]
+for sample in samples:
+    print(len(sample))
